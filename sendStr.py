35bcea98551d512339ad2a9bdb78e23153623bf0
Y.Nakamura <gp20a110@oecu.jp>
2023-04-18 20:46:32 +0900

    Change URI

--- sendStr.py
+++ sendStr.py
@@ -7,11 +7,8 @@ import os
 
 app = FastAPI()
 
-
-
-
 # getで受け取った文字列をM5stickに送信する
-@app.get("/{str}")
+@app.get("/send/{str}")
 def sendtr(str: str):
     ser = serial.Serial('/dev/tty.usbserial-59529F6CAB', 115200, timeout=1)
     ser.write(str.encode())
