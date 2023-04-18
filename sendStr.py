7fabdf2f8282046dea5dd7bdf18e0d15247628c6
Y.Nakamura <gp20a110@oecu.jp>
2023-04-18 20:28:24 +0900

    Add the code that works for now

---
+++ sendStr.py
@@ -0,0 +1,18 @@
+# WebAPIを動作させるためのライブラリ
+from fastapi import FastAPI
+import uvicorn
+# M5stickとシリアル通信を行うためのライブラリ
+import serial
+import os
+
+app = FastAPI()
+
+
+
+
+# getで受け取った文字列をM5stickに送信する
+@app.get("/{str}")
+def sendtr(str: str):
+    ser = serial.Serial('/dev/tty.usbserial-59529F6CAB', 115200, timeout=1)
+    ser.write(str.encode())
+    return {"message": "success"}
