665a18ebd70c1f0bf8bd0bc4326a88a1660adbd0
Y.Nakamura <gp20a110@oecu.jp>
2023-04-20 15:23:00 +0900

    Add init.sh and uvicorn.service

---
+++ setup/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Modify : 2023/04/19 v1.0.0
+# Author : @mu7889yoon
+
+# Update Package
+sudo apt update
+sudo apt install -y git python3-pip cockpit
+git clone https://github.com/mu7889yoon/py2m5lcd.git
+cd py2m5lcd
+pip3 install -r requirements.txt
+cd setup
+sudo cp uvicorn.service /etc/systemd/system/
+sudo systemctl enable uvicorn.service
+sudo systemctl start uvicorn.service
+sudo cp cockpit.service /etc/systemd/system/
+sudo systemctl enable cockpit.service
+sudo systemctl start cockpit.service
+
+
+
