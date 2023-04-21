11ee33a5b15575112910bcf1121261f1dc286e82
Y.Nakamura <gp20a110@oecu.jp>
2023-04-21 16:39:35 +0900

    Add a code to get ID of m5stick

--- sendStr.py
+++ sendStr.py
@@ -7,9 +7,21 @@ import os
 
 app = FastAPI()
 
+# M5stickのシリアルポート番号を取得する
+def getM5Tty():
+    for i in range(30):
+        usbSerial = '/dev/ttyUSB{}'.format(i)
+        if(os.path.exists(usbSerial)):
+            print(usbSerial)
+            return usbSerial
+    return None
+
+ttyNo = getM5Tty()
+
 # getで受け取った文字列をM5stickに送信する
 @app.get("/send/{str}")
 def sendtr(str: str):
-    # ser = serial.Serial('/dev/tty.usbserial-59529F6CAB', 115200, timeout=1)
-    # ser.write(str.encode())
-    return {"message": str}
+    ser = serial.Serial(ttyNo, 115200, timeout=1)
+    ser.write(str.encode())
+    return {"strings": str}
+
