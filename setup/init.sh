b5b26166fa39294bace6d8e71373323daf36312c
Y.Nakamura <gp20a110@oecu.jp>
2023-04-21 16:41:00 +0900

    Add a comments

--- setup/init.sh
+++ setup/init.sh
@@ -6,15 +6,19 @@
 # Update Package
 sudo apt update
 sudo apt install -y git python3-pip cockpit
+# Clone Repository
 git clone -b develop https://github.com/mu7889yoon/py2m5lcd.git
 cd py2m5lcd
+# Install Package
 pip3 install -r requirements.txt
 cd setup
+# Setup Service
 sudo cp uvicorn.service /etc/systemd/system/
 sudo systemctl enable uvicorn.service
 sudo systemctl start uvicorn.service
 sudo systemctl enable cockpit.service
 sudo systemctl start cockpit.service
+# Test LCDにSetup completeを表示
 curl http://localhost:8080/send/Setup%20Complete
 
 
